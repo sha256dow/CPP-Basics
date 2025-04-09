249d50e122126dc685500d970b2c4adb72104272
Teuz <96244097+sha256dow@users.noreply.github.com>
2025-04-09 20:49:55 -0300

    hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+// This is just a "Hello World" introduction file.
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World" << endl;
+
+return 0;
+
+}
+
+
+// This is just a "Hello World" introduction file.
